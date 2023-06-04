640985035975ac4f32508c51415b9a25ceed9287
hp i5 7th <harshitagalani59@gmail.com>
2023-06-04 12:26:09 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(title: Text("good moorning"),),);
   }
 }
